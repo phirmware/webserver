f1819b453f673d42e41b60ebecf80a0497516c3e
phirmware <chibuzor.ojukwu@gmail.com>
2020-05-13 13:42:10 +0100

    added login view and authentication logic

--- controllers/users.go
+++ controllers/users.go
@@ -11,8 +11,9 @@ import (
 
 // Users defines the users controller
 type Users struct {
-	NewView *views.View
-	us      *models.UserService
+	NewView   *views.View
+	LoginView *views.View
+	us        *models.UserService
 }
 
 // SignupForm defines the shape of the users input
@@ -22,11 +23,18 @@ type SignupForm struct {
 	Password string `schema:"password"`
 }
 
+// LoginForm defines the shape of the login form
+type LoginForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 // NewUsers Renders our new users page
 func NewUsers(us *models.UserService) *Users {
 	return &Users{
-		NewView: views.NewView("bootstrap", "users/new"),
-		us:      us,
+		NewView:   views.NewView("bootstrap", "users/new"),
+		LoginView: views.NewView("bootstrap", "users/login"),
+		us:        us,
 	}
 }
 
@@ -37,13 +45,63 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Login handles the GET /login
+func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
+	if err := u.LoginView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+// .x3RFTVjGyA9SMysDJVtDW6ttZK
+
 // Create is the controller that creates a new user
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	form := SignupForm{}
 	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
-	fmt.Fprintln(w, "Email is", form.Email)
-	fmt.Fprintln(w, "Password is", form.Password)
-	fmt.Fprintln(w, "Name is", form.Name)
+	user := models.User{
+		Name:     form.Name,
+		Email:    form.Email,
+		Password: form.Password,
+	}
+	if err := u.us.Create(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+// HandleLogin handles the request to login from the user
+func (u *Users) HandleLogin(w http.ResponseWriter, r *http.Request) {
+	form := LoginForm{}
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	user, err := u.us.Authenticate(form.Email, form.Password)
+	if err != nil {
+		switch err {
+		case models.ErrNotFound:
+			fmt.Fprintln(w, "Invalid email address.")
+		case models.ErrInvalidPassword:
+			fmt.Fprintln(w, "Invalid password provided.")
+		default:
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+	cookie := http.Cookie{
+		Name:  "email",
+		Value: user.Email,
+	}
+	http.SetCookie(w, &cookie)
+	fmt.Fprintln(w, user)
+}
+
+func (u *Users) CookieTest(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("email")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, "Your email cookie value is : ", cookie.Value)
 }
