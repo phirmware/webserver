6b1a4f092c6aca7675593fc4f0a15d701543d267
phirmware <chibuzor.ojukwu@gmail.com>
2020-06-08 03:48:42 +0100

    improved postgres setup script

--- postgres/setupDB.sh
+++ postgres/setupDB.sh
@@ -1,9 +1,34 @@
 #! /usr/bin/env bash
 
+function checkDB() {
+    go run main.go
+    if [ $? -eq 0 ]; then
+       echo "DB was successfully setup, Happy coding"
+    else
+       echo "Something went wrong, try running go file"
+    fi
+}
+
 docker rmi webdevgo
+if [ $? -eq 0 ]; then
+   echo "Successfully removed Image"
+else
+   echo "Image not found, creating image"
+fi
 
 docker build -f dockerfile -t webdevgo .
+if [ $? -eq 0 ]; then
+   echo "Successfully Built docker image"
+else
+   echo "Something went wrong"
+fi
 
 docker run --rm -d  -p 5432:5432 -e POSTGRES_DB=lenslocked_dev webdevgo:latest
-
-echo "Run GO file main.go TO CHECK IF DB WAS SUCCESSFULLY CREATED"
+if [ $? -eq 0 ]; then
+   echo "Postgres Container running succesfully"
+   echo "Checking db connectivity, please wait........."
+   sleep 4
+   checkDB
+else
+   echo "Something went wrong"
+fi
